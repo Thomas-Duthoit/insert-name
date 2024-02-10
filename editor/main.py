b784bbfea961091d53e19ddf0a812b49ab536765
Zatomate <duthoit.thomas9@gmail.com>
2024-02-10 14:40:02 +0100

    adding window name

--- editor/main.py
+++ editor/main.py
@@ -10,21 +10,23 @@ import time
 # CLASSES
 class Editor:  # main class for the editor
     def __init__(self):  # class init
-        self.TIME_FLAG = time.time()  # used by the log function
-        self.WS = (800, 600)  # window size: (width, height)
+        self.TIME_FLAG = time.time()  # CONST: timestamp used by the log function
+        self.WS = (800, 600)  # CONST: window size: (width, height)
+        self.WN = 'MAP MAKER v0.1'  # CONST: window name
 
-        self.log("Création de la fenêtre")
+        self.log('Creating window')
         self.root = pygame.display.set_mode(self.WS)  # creating the pygame window
+        pygame.display.set_caption(self.WN)  # setting the window name to 'self.WN'
         self.running = True  # variable responsable for "mainloop while condition"
         self.run()  # actualy starting the editor
         pygame.quit()  # clean pygame exit
-        self.log("Fermeture de l'éditeur")
+        self.log('Closing editor')
 
     def log(self, msg):  # method to log a message into console with the corresponding timestamp
-        print(f"{int((time.time()-self.TIME_FLAG)*100)/100}] {msg}")
+        print(f'{int((time.time()-self.TIME_FLAG)*100)/100}] {msg}')
 
     def render(self):  # method responsable for the rendering (visual part.)
-        self.root.fill("black")  # resetting the root with black
+        self.root.fill('black')  # resetting the root with black
         # TODO: implement visual part
 
     def update(self):  # method responsable for the update (logical part.)
